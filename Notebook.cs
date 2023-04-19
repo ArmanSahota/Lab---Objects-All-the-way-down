7a2e70ec46d48c261ecaf2ec2b0d22b9b2616e61
ArmanSahota <109508805+ArmanSahota@users.noreply.github.com>
2023-04-19 14:09:27 -0700

    Add project files.

---
+++ Notebook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Lab___Objects__All_the_way_down.Notebook;
+
+namespace Lab___Objects__All_the_way_down
+{
+    internal class Notebook
+    {
+        public class Notebooks
+        {
+            public Covers.CoverType Cover { get; set; }
+            public Paper.PaperType Paper { get; set; }
+
+            public Notebooks(Covers.CoverType cover, Paper.PaperType paper)
+            {
+                Cover = cover;
+                Paper = paper;
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+using Lab___Objects__All_the_way_down;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Notebook.Notebooks notebook1 = new Notebook.Notebooks(Covers.CoverType.Spiral, Paper.PaperType.CollegeRule);
+        Notebook.Notebooks notebook2 = new Notebook.Notebooks(Covers.CoverType.Scientific, Paper.PaperType.GraphPaper);
+        Notebook.Notebooks notebook3 = new Notebook.Notebooks(Covers.CoverType.BulletJournals, Paper.PaperType.MonthlyCalendarPaper);
+
+        Console.WriteLine("Notebook 1: " + notebook1.Cover + " cover, " + notebook1.Paper + " paper");
+        Console.WriteLine("Notebook 2: " + notebook2.Cover + " cover, " + notebook2.Paper + " paper");
+        Console.WriteLine("Notebook 3: " + notebook3.Cover + " cover, " + notebook3.Paper + " paper");
+
+        Console.ReadLine();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
